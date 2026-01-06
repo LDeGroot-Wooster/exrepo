dedf82b61f37afb57a1e6e5c71e7738f966a1ba5
LDeGroot-Wooster <ldegroot@wooster.edu>
2026-01-06 16:44:54 -0500

    editing indents

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python
-    import os
-    import unittest
+import os
+import unittest
 
-    WORKING_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    print(WORKING_DIR)
+WORKING_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(WORKING_DIR)
 
-    import sys
+import sys
 
-    sys.path.insert(1, os.path.join(WORKING_DIR, "exrepo"))
-    from exrepo.core import random_walk, ornstein_uhlenbeck
+sys.path.insert(1, os.path.join(WORKING_DIR, "exrepo"))
+from exrepo.core import random_walk, ornstein_uhlenbeck
 
 
-    class TestModels(unittest.TestCase):
+class TestModels(unittest.TestCase):
+    """
+    Tests integration and models
+    """
+    def test_random_walk(self):
         """
-        Tests integration and models
+        Test generating a trajectory
         """
-        def test_random_walk(self):
-            """
-            Test generating a trajectory
-            """
-            traj = random_walk(0.1, 100)
-            assert len(traj) == 100, "Generated trajectory has the wrong shape"
+        traj = random_walk(0.1, 100)
+        assert len(traj) == 100, "Generated trajectory has the wrong shape"
             
-        def test_ornstein_uhlenbeck(self):
-            """
-            Test generating a trajectory with stochasticity
-            """
-            traj = ornstein_uhlenbeck(0.1, 100)
-            assert len(traj) == 100, "Generated trajectory has the wrong shape"
+    def test_ornstein_uhlenbeck(self):
+        """
+        Test generating a trajectory with stochasticity
+        """
+        traj = ornstein_uhlenbeck(0.1, 100)
+        assert len(traj) == 100, "Generated trajectory has the wrong shape"
 
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
 
